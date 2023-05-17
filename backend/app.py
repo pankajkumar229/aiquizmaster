2c0e917b0f3aaa0df06619dae3d3a8869e90d8c3
nrghosh <nrghosh@wesleyan.edu>
2023-05-17 13:06:58 -0700

    keep api key in env, tweak prompt and use 3.5-turbo model

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,44 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import requests
+import openai
+import os
 
 app = Flask(__name__)
 
+# Set up OpenAI API credentials 
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Read the parsed text 
+def read_text_file(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+    return text
+
 # Use ChatGPT API to generate quiz questions (and answers)
+# Generate quiz question and answer using OpenAI ChatGPT API
 def generate_quiz():
     try:
-        headers = {
-            'Authorization': 'Bearer INSERT_OPENAI_KEY_HERE', # TODO secrets management - github will close repo if we accidentally send openai api key
-            'Content-Type': 'application/json'
-        }
+        text_file_path = 'path/to/your/text/file.txt'
+        text = read_text_file(text_file_path)
 
-
-        # Assume OCR / parser has processed input pdf file and dumped results to a local dir. Use this as context for ChatGPT API call + quiz generation
-        with open('path/to/file.txt', 'r') as file:
-            text = file.read()
-
-        data = {
-            'messages': [
-                {'role': 'system', 'content': 'You are an educational quiz bot. Given the following context, \
-                 come up with a question concerning core ideas or details included in the text. Ensure that the \
-                 question is directly relevant and answerable based solely on information provided in the text'},
+        response = openai.Completion.create(
+            engine='gpt-3.5-turbo',
+            messages=[
+                {'role': 'system', 'content': 'You are an educational quiz bot. Given the following context, come up with a question \
+                 concerning core themes, central ideas, or specific details included in the text. Ensure that the question is directly \
+                 relevant and answerable based solely on information provided in the text.'},
                 {'role': 'user', 'content': 'Generate a quiz question and answer.'},
-                {'role': 'assistant', 'content': text}  # text file content as context for questions
+                {'role': 'assistant', 'content': text}  # Add the text file content as context
             ],
-            'max_tokens': 50
-        }
+            max_tokens=50
+        )
 
-        response = requests.post('https://api.openai.com/v1/chat/completions', json=data, headers=headers)
-        response.raise_for_status()
-
-        choices = response.json()['choices']
+        choices = response['choices']
         question = choices[0]['message']['content']
         answer = choices[1]['message']['content'] if choices[0]['message']['role'] == 'assistant' else choices[0]['message']['content']
 
         return {'question': question, 'answer': answer}
-    except requests.exceptions.RequestException as e:
+    except openai.Error as e:
         print('Failed to generate quiz:', str(e))
         raise e
 
@@ -44,7 +47,7 @@ def handle_generate_quiz():
     try:
         quiz = generate_quiz()
         return jsonify(quiz)
-    except Exception as e:
+    except requests.Exception as e:
         return jsonify({'error': 'Failed to generate quiz.'}), 500
 
 @app.route('/api/collect-feedback', methods=['POST'])
