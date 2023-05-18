4fa00194c53d1d732a8d9d260a10acf01fa592e0
pankaj kumar <pankaj@datacabinet.systems>
2023-05-18 00:35:48 -0700

    All file gets working

--- backend/app.py
+++ backend/app.py
@@ -1,59 +1,74 @@
-from flask import Flask, jsonify
-import requests
-import openai
-import os
+import tornado
+import tornado.ioloop
+import tornado.web
+import os, uuid, json
+import asyncio
+import aiofiles
+from tornado.web import MissingArgumentError, HTTPError, Application, StaticFileHandler
 
-app = Flask(__name__)
+__UPLOADS__ = "uploads/"
 
-# Set up OpenAI API credentials 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+class Userform(tornado.web.RequestHandler):
+    def get(self):
+        self.render("fileuploadform.html")
 
-# Read the parsed text 
-def read_text_file(file_path):
-    with open(file_path, 'r') as file:
-        text = file.read()
-    return text
+docmap = {}
 
-# Use ChatGPT API to generate quiz questions (and answers)
-# Generate quiz question and answer using OpenAI ChatGPT API
-def generate_quiz():
-    try:
-        text_file_path = 'path/to/your/text/file.txt'
-        text = read_text_file(text_file_path)
+class DefaultHandler(tornado.web.RequestHandler):
+    ## Test with $ curl -F 'filearg=@/home/labcomputer/aiquizmaster/README.md' http://localhost:8888/backend/doc
+    async def get(self):
+        self.write(json.dumps({}))
 
-        response = openai.Completion.create(
-            engine='gpt-3.5-turbo',
-            messages=[
-                {'role': 'system', 'content': 'You are an educational quiz bot. Given the following context, come up with a question \
-                 concerning core themes, central ideas, or specific details included in the text. Ensure that the question is directly \
-                 relevant and answerable based solely on information provided in the text.'},
-                {'role': 'user', 'content': 'Generate a quiz question and answer.'},
-                {'role': 'assistant', 'content': text}  # Add the text file content as context
-            ],
-            max_tokens=50
-        )
 
-        choices = response['choices']
-        question = choices[0]['message']['content']
-        answer = choices[1]['message']['content'] if choices[0]['message']['role'] == 'assistant' else choices[0]['message']['content']
+class DocHandler(tornado.web.RequestHandler):
+    ## Test with $ curl -F 'filearg=@/home/labcomputer/aiquizmaster/README.md' http://localhost:8888/backend/doc
+    async def post(self):
+        #https://technobeans.com/2012/09/17/tornado-file-uploads/
+        fileinfo = self.request.files['filearg'][0]
+        print ("fileinfo is", fileinfo)
+        fname = fileinfo['filename']
+        extn = os.path.splitext(fname)[1]
+        uuid_str = str(uuid.uuid4())
+        cname = uuid_str + extn
+        async with aiofiles.open(__UPLOADS__ + cname, mode='wb') as f:
+            await f.write(fileinfo['body'])
+        docmap[uuid_str] = {"fullname":fname, "extn": extn, "cname": cname, "id":uuid_str}
+        print(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
+        self.write(json.dumps(docmap[uuid_str]))
 
-        return {'question': question, 'answer': answer}
-    except openai.Error as e:
-        print('Failed to generate quiz:', str(e))
-        raise e
+    async def get(self, did = None):
+        if self.get_query_argument('file',None):
+            ### Test with curl http://localhost:8888/backend/doc/af50cf65-5869-4b22-a15e-e6dd41b7e3a5/?file=True
+            chunkSize = 1024 * 1024 * 1 # 1 Mib
+            with open(__UPLOADS__ + docmap[did]["cname"], 'rb') as f:
+                while True:
+                    chunk = f.read(chunkSize)
+                    if not chunk:
+                        break
+                    try:
+                        self.write(chunk) # write the chunk to the response
+                        await self.flush()# send the chunk to the client
+                    except iostream.StreamClosedError:
+                        break
+                    finally:
+                        del chunk
+                        await gen.sleep(0.000000001)
+            self.finish()
+        else:
+            ### Test with curl http://localhost:8888/backend/doc/7e5cc450-ad6b-4475-9cbd-859a02d28e5e/
+            self.write(json.dumps(docmap[did]))
 
-@app.route('/api/generate-quiz', methods=['POST'])
-def handle_generate_quiz():
-    try:
-        quiz = generate_quiz()
-        return jsonify(quiz)
-    except requests.Exception as e:
-        return jsonify({'error': 'Failed to generate quiz.'}), 500
 
-@app.route('/api/collect-feedback', methods=['POST'])
-def handle_collect_feedback():
-    # TODO: handle user feedback (just text?)
-    return jsonify({'message': 'Feedback received.'})
+async def main():
+    application = Application([
+        (r"/", DefaultHandler),
+        (r"/backend/doc", DocHandler),
+        (r"/backend/doc/(?P<did>.+)/", DocHandler),
+    ], autoreload=True)
+    application.listen(8888)
+    shutdown_event = asyncio.Event()
+    print("Listening on 8888")
+    await shutdown_event.wait()
 
-if __name__ == '__main__':
-    app.run(port=3000)
+if __name__ == "__main__":
+    asyncio.run(main())
